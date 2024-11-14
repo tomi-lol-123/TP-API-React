7b84bf8c2e027b39bd29c17f9e1387a82fe0c366
tomi-lol-123 <153372634+tomi-lol-123@users.noreply.github.com>
2024-11-14 10:44:58 -0300

    feat: cambio link de React por "tetas o culos?"

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ¿Tetas o culo?
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?app=desktop&v=G6WsU2JIoBQ"
           target="_blank"
           rel="noopener noreferrer"
         >
